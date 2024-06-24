a6d9904521b96a29e5084f3e93ef2c86cdb92b2b
Matt Hunzinger <matthunz2@gmail.com>
2024-06-23 22:17:44 -0400

    Create Codec monad

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,29 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE GADTs #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module Lib
+  (
+  )
+where
+
+import Data.Aeson (ToJSON (toJSON), Value)
+
+data Item d o a where
+  IntI :: Item d o Int
+
+data Codec d o a where
+  PureS :: a -> Codec d o a
+  ItemS :: (d -> a) -> Item d o a -> Codec d o a
+  BindS :: Codec d o b -> (b -> Codec d o a) -> Codec d o a
+
+intC :: (d -> Int) -> Codec d o Int
+intC f = ItemS f IntI
+
+instance Functor (Codec d o) where
+  fmap f c = BindS c (pure . f)
+
+instance Applicative (Codec d o) where
+  pure = PureS
+  cf <*> ca = BindS cf (`fmap` ca)
+
+instance Monad (Codec d o) where
+  (>>=) = BindS
